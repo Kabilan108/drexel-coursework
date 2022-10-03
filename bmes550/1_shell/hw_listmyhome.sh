1e64795d99393d01e566f31589ae59a550d6b504
ApeMaster <tonykabilanokeke@gmail.com>
2022-10-03 11:38:55 -0400

    Update code with awk
    Now using awk to extract only the file name, size and access permissions

--- bmes550/1_shell/hw_listmyhome.sh
+++ bmes550/1_shell/hw_listmyhome.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Write a bash script that goes into the home directory, lists files 
-# in that directory, with file sizes and access permissions, then
-# go back to the original directory you started with.
-
+# List the name, file size, and access permissions for non-empy files in 
+# the home directory.
 
 old_dir=$(pwd)
 cd ~
-ls -lhA
+ls -lhA | \
+    awk -v FS=' ' -v OFS='\t' '
+        BEGIN{ print "PERMISSIONS \t SIZE \t FILENAME" } 
+        NR>1{ printf "%-11s \t %-5s \t %-10s\n", $1, $5, $9 }
+    '
 cd $old_dir
