5185f8ece4e0a2e7ae104a77e29df8f6a640e1bc
ApeMaster <tonykabilanokeke@gmail.com>
2022-10-03 11:39:45 -0400

    Update code

--- bmes550/1_shell/hw_backupself.sh
+++ bmes550/1_shell/hw_backupself.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Write a bash script file that creates a backup copy of itself (append 
-# .bak to whatever its own name is). e.g., backupself.sh should create
-# a backup of itself called backupself.sh.bak. 
-# If you run backupself.sh.bak it should create backupself.sh.bak.bak
+# Back up this file to $0.bak
 
 cp $0 "$0.bak"
