eb4cb54ae5055ea9f18c9effe9ef5d10e0e98f34
ApeMaster <tonykabilanokeke@gmail.com>
2022-11-21 18:44:26 -0500

    Complete tasks for assignment

--- bmes550/6_image/homework/hwbmeimg_countcells.py
+++ bmes550/6_image/homework/hwbmeimg_countcells.py
@@ -4,15 +4,16 @@ Functions for Image Processing
 By Tony Kabilan Okeke <mailto:tko35@drexel.edu>
 """
 
-# Import the required libraries
-import matplotlib.pyplot as plt
-import skimage.io as io
-import numpy as np
-
-# Import the bmes module
+# Import the bmes package
 import sys, os
 sys.path.append(os.environ['BMESAHMETDIR'])
 import bmes
+
+# Import necessary packages
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import io, color, filters,  morphology, measure
+from scipy import ndimage
 
 
 def myimshow(img: np.ndarray, title: str='', dbg: bool=True) -> None:
@@ -24,37 +25,77 @@ def myimshow(img: np.ndarray, title: str='', dbg: bool=True) -> None:
         io.imshow(img)
         if title: plt.title(title)
         io.show();
+        
 
-
-def hwbmeimg_countcells(file: str='', dbg: bool=False) -> int:
+def bmeimg_countcells(file: str='', dbg: bool=True, edge_thr: float=0.1) -> int:
     """
     Count the cells in a microscopy image
+
+    Parameters
+    ----------
+    file : str
+        The file name of the image to be processed
+    dbg : bool
+        Whether to show the intermediate steps
+    edge_thr : float
+        The threshold for the edge detection (used to convert the Sobel edge
+        image to a binary image)
     """
 
-    # If no file is given, download the sample image
+    # If no file is specified, use the default file
     if not file:
         URL = ('http://sacan.biomed.drexel.edu/lib/exe/fetch.php'
                '?media=course:bcomp2:img:samplecells.jpg')
-        file = bmes.downloadurl(URL, './samplecells.jpg')
-    
-    # Read the image data from the file
-    img = io.imread(file)
-    myimshow(img, 'Original Image', dbg);
+        file = bmes.downloadurl(URL, './samplecells.jpg');
 
-    # Apply sobel edge detection
+    # Read the image
+    I = io.imread(file)
 
-    # Dilate the edge image
+    # Convert to grayscale for processing
+    G = color.rgb2gray(I)
 
-    # Fill the interior gaps of the edge image
+    # Edge detection (Sobel)
+    E = filters.sobel(G, mode='constant')
+    # Convert to a binary image
+    E = np.where(E > edge_thr, 1, 0).astype(np.float64)
 
-    # Smoothen the objects in the edge image
+    # Image dilation
+    D = morphology.dilation(E, morphology.diamond(5))
 
-    # Show the edge image overlayed on the original image
+    # Gap filling
+    seed = D.copy()
+    seed[1:-1, 1:-1] = D.max()
+    F = morphology.reconstruction(seed, D, method='erosion')
+
+    # Smoothening (erosion)
+    S = morphology.erosion(F, morphology.square(2)).astype(np.float64)
+
+    # Count the number of objects
+    L, _ = ndimage.label(S)  # type: ignore
+
+    # Remove small objects
+    blobs = measure.regionprops(L)
+    blobs = [b for b in blobs if b.area > 200]
+    n = len(blobs)
+
+    # Construct a labeled image
+    L = np.zeros(L.shape)
+    for i, b in enumerate(blobs, 1):
+        L[b.coords[:,0], b.coords[:,1]] = i
+    L = L.astype(np.int32)
+
+    # Overlay the labeled image on the original image
+    O = color.label2rgb(L, I, bg_label=0)
+
+    # Display the results
     if dbg:
-        pass
+        fig, axes = plt.subplots(2, 3, figsize=(12, 8))
+        axes = axes.flatten()  # type: ignore
+        titles = ['Original Image', 'Edge Detection (Sobel)', 'Dilated Image', 
+                'Filled Edge Image', 'Smoothened Image', 'Overlay']
+        for ax, img, title in zip(axes, [I, E, D, F, S, O], titles):
+            ax.imshow(img, cmap='gray' if img.ndim == 2 else None)
+            ax.set_title(title)
+        plt.tight_layout()
 
-    # Find the connected components in the edge image
-
-    # Count the number of cells in the image
-
-    return 0
+    return n
