147c4093bf16ba06a48319d6d02f3eb14d8b4f80
Tony Okeke <tonykabilanokeke@gmail.com>
2023-02-27 00:56:01 -0500

    Aff loadcatsite function

--- bmes547/6_nn/homework/loadcatsite.py
+++ bmes547/6_nn/homework/loadcatsite.py
@@ -3,8 +3,36 @@ Author: Tony Kabilan Okeke <tko35@drexel.edu>
 template by: AhmetSacan.
 """
 
-# Imports
-import os
+# Imports for data loading and manipulation
+import scipy.io as sio
+import pandas as pd
+import numpy as np
+
+# Import the bmes module
+import sys, os; sys.path.append(os.environ['BMESAHMETDIR'])
+import bmes
+
+
+# Amino acid conventional ordering
+aminos = 'ARNDCQEGHILKMFPSTWYV'
+
+
+def OneHotEncoder(aaseq):
+    """
+    Function to convert a single amino acid into a one-hot encoding.
+    """
+
+    # Initialize a 20-element vector of zeros.
+    onehot = np.zeros((len(aaseq), 20))
+
+    # Set the appropriate elements to 1
+    for i in range(len(aaseq)):
+        if aaseq[i] in aminos:
+            onehot[i, aminos.index(aaseq[i])] = 1
+        else:
+            onehot[i, :] = 0
+
+    return onehot
 
 
 def loadcatsite() -> tuple:
@@ -24,14 +52,19 @@ def loadcatsite() -> tuple:
         A list containing the names of the attributes in the catsite dataset.
     """
 
-    # Lad the NataliaPetrova.catsite.arff file into variable catsite
-    # Read in the NataliaPetrova.catsite.arff file using the provided bmes.arffload() function
-    # (which was slightly modified from "dataformat project":
-    # https://ml01.zrz.tu-berlin.de/trac/dataformat/browser/trunk/matlab/arffload.m )
-    file=bmes.downloadurl('http://sacan.biomed.drexel.edu/lib/exe/fetch.php?rev=&media=course:ml:nn:hwnn.catsite:NataliaPetrova.catsite.arff');
-    catsite=bmes.arffload(file);
+    # Load the NataliaPetrova.catsite.arff file into variable catsite
+    URL = ('http://sacan.biomed.drexel.edu/lib/exe/fetch.php?rev=&media=course:'
+           'ml:nn:hwnn.catsite:NataliaPetrova.catsite.arff')
+    file = bmes.downloadurl(URL, 'catsite.arff');
+    data, _ = sio.arff.loadarff(file)
+    df = pd.DataFrame(data)
 
+    # Decode any bytes to strings
+    df = df.applymap(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
 
+    # Extract numeric features
+    X = df.select_dtypes(include=['float64', 'int64']).values
+    
     # Convert the AAName1LetterCode feature to numeric data.
     # Each amino acid should be represented by 20 binary numbers. You must use
     # the conventional ordering of the amino acids. See int2aa() for the
@@ -43,18 +76,26 @@ def loadcatsite() -> tuple:
     # Assume that only standard amino acids (one of: 'ARNDCQEGHILKMFPSTWYV')
     # will appear in the dataset. With this assumption, non-standard amino
     # acids, if any, would end up being represented with all-zeros in your
-    # one-hot encoding.  
+    # one-hot encoding.
+    A = OneHotEncoder(df['AAName1LetterCode'].values)
 
+    # Add the one-hot encoding of the amino acid sequence to the feature matrix X.
+    X = np.hstack((A, X))
+
+    # Create names for the features.
+    names = ['AminoAcid' + aminos[i] for i in range(20)]
+    names.extend(df.select_dtypes(include=['float64', 'int64']).columns.values)
 
     # Remove data with missing values.
     # Remove all data rows where a numeric value is NaN (not a number), which
     # are indicated by '?' in the arff file. See matlab function ``isnan()''.
-
+    X = X[~np.isnan(X).any(axis=1)]
 
     # Convert the class labels to numeric +1/-1.
     # Store the class labels into a numerical vector T. Remove that column from X.
-
+    T = df['class'].apply(eval).values
 
     # Convert the data into a numeric matrix X, if you haven't already.
     # X: one row per data sample, and one column for each feature.
 
+    return X, T, names
